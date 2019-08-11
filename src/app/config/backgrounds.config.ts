c0810b9ccaddd0c04ae849b2cae955d62844d643
janssenb <bram.janssen@vito.be>
2019-08-11 21:46:11 +0200

    Removed background layer

--- src/app/config/backgrounds.config.ts
+++ src/app/config/backgrounds.config.ts
@@ -21,15 +21,6 @@ export const BACKGROUNDS = [
     })
   },
   {
-    name: 'background_osm_hike_bike',
-    title: 'OSM Hike & Bike',
-    layer: new TileLayer({
-      source: new XYZ({
-        url: 'https://a.tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png'
-      })
-    })
-  },
-  {
     name: 'background_Outdoors',
     title: 'Outdoors',
     layer: new TileLayer({

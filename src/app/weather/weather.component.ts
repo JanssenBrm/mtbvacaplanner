9ba26cdfb79ea659627a8f9982523ab08cc43114
janssenb <bram.janssen@vito.be>
2019-08-11 21:49:33 +0200

    Weather fix

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -25,7 +25,6 @@ export class WeatherComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     var s = document.createElement("script");
-
     s.type = "text/javascript";
     s.src = `https://darksky.net/widget/default/${this.activeRoute.ride.center[1]},${this.activeRoute.ride.center[0]}/uk12/en.js?width=100%&height=${this.height + 25}&textColor=333333&bgColor=eceff4&transparency=true&skyColor=undefined&fontFamily=Default&customFont=&units=uk&htColor=333333&ltColor=C7C7C7&displaySum=yes&displayHeader=no`;
     this.elementRef.nativeElement.appendChild(s);

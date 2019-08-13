d4b510a31242ae9d950f82e1ff674dc4ce56e80f
janssenb <bram.janssen@vito.be>
2019-08-13 19:19:38 +0200

    Cleanup of routes

--- src/app/config/routes.config.ts
+++ src/app/config/routes.config.ts
@@ -1,5 +1,8 @@
 export const ROUTES = [
   {
+    path: 'assets/files/2012-05-01_86035678_Panoramic Tour (EDIT).gpx',
+    info: 'https://www.komoot.com/tour/86035678'
+  },{
     path: 'assets/files/bergfextour_haiming-haiming--sattele--oetz.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/1420,haiming-haiming--sattele--oetz/'
   },
@@ -22,41 +25,19 @@ export const ROUTES = [
   {
     path: 'assets/files/bergfextour_mtb--plateaurunde--suedlicher-bereich.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/2181,mtb--plateaurunde--suedlicher-bereich/'
-  },{
+  }, {
     path: 'assets/files/bergfextour_mtb-oetz--umhausen--schweinfurter-huette--stuibenfall.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/195303,mtb-oetz--umhausen--schweinfurter-huette--stuibenfall/'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85599104_GPX Download_ Oetz – Piburger See Loop from Kühtaistraße.gpx',
     info: 'https://www.komoot.com/smarttour/598648#og%23'
-  },{
-    path: 'assets/files/2019-08-11_85599293_GPX Download_ Gasthof Stuibenfall – Niederthai Loop from Oetz.gpx',
-    info: 'https://www.komoot.com/smarttour/764960#og%23'
-  },{
-    path: 'assets/files/2019-08-11_85599422_GPX Download_ Sautens – Kneipp Anlage zum Abkühlen Loop from Oetz.gpx',
-    info: 'https://www.komoot.com/smarttour/492828#og%23'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85599628_GPX Download_ Piburger See – Oetz Loop from Start.gpx',
     info: 'https://www.komoot.com/smarttour/437984#og%23'
-  },{
-    path: 'assets/files/2019-08-11_85600013_GPX Download_ Area 47 – Sautens Loop from Haiming.gpx',
-    info: 'https://www.komoot.com/smarttour/598904#og%23'
-  },{
-    path: 'assets/files/2019-08-11_85600213_GPX Download_ Gasthof Stuibenfall – Niederthai Loop from Start.gpx',
-    info: 'https://www.komoot.com/smarttour/433462#og%23'
   },
   {
     path: 'assets/files/t7727874_bike route - tour of 5 villages.gpx',
     info: 'https://www.outdooractive.com/mobile/en/route/mountain-bike/oetztal/bike-route-tour-of-5-villages/7727874/'
-  },{
-    path: 'assets/files/t7726448_bike route - baerentalhuette.gpx',
-    info: 'https://www.outdooractive.com/mobile/en/route/mountain-bike/oetztal/bike-route-baerentalhuette/7726448/'
-  },{
-    path: 'assets/files/2019-08-11_85603408_GPX Download_ Umhausen – Klettergarten Engelswand Loop from Umhausen.gpx',
-    info: 'https://www.komoot.com/smarttour/764947#og%23'
-  },
-  {
-    path: 'assets/files/2019-08-11_85603809_GPX Download_ Umhausen – Niederthai Loop from Start.gpx',
-    info: 'https://www.komoot.com/smarttour/318021#og%23'
   },
   {
     path: 'assets/files/bergfextour_mtb-laengenfeld--gries--vordere-sulztalalm--nisslalm--kurz-vor-amberger-huette.gpx',
@@ -73,118 +54,78 @@ export const ROUTES = [
   {
     path: 'assets/files/bergfextour_biketour-zur-schweinfurter-huette-601.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/45993,biketour-zur-schweinfurter-huette-601/'
-  },{
+  }, {
     path: 'assets/files/bergfextour_biketour-zur-amberger-huette-694.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/46016,biketour-zur-amberger-huette-694/'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85604973_GPX Download_ Breitlehnalm – Polltalalm Loop from Start.gpx',
     info: 'https://www.komoot.com/smarttour/433550#og%23'
-  },{
-    path: 'assets/files/2019-08-11_85605055_GPX Download_ Ebme Line – Gaißlachmittelstation Loop from Längenfeld.gpx',
-    info: 'https://www.komoot.com/smarttour/764473#og%23'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85605301_GPX Download_ Mountainbike Loop from Huben.gpx',
     info: 'https://www.komoot.com/smarttour/633547#og%23'
-  },{
-    path: 'assets/files/2019-08-11_85605434_GPX Download_ EBikeHow Workshop Ötztal – Längenfeld Loop from Aquadome.gpx',
-    info: 'https://www.komoot.com/smarttour/764926#og%23'
-  },
-  {
-    path: 'assets/files/t7726550_niederthai-schweinfurter huette bike route.gpx',
-    info: 'https://www.outdooractive.com/mobile/en/route/mountain-bike/oetztal/niederthai-schweinfurter-huette-bike-route/7726550/'
   },
   {
     path: 'assets/files/t7726800_route - round around rechenstielegg -684-.gpx',
     info: 'https://www.outdooractive.com/mobile/en/route/mountain-bike/oetztal/route-round-around-rechenstielegg-684-/7726800/'
   },
   {
-    path: 'assets/files/2019-08-11_85605852_GPX Download_ EBikeHow Workshop Ötztal – Amberger-Hütte Loop from Ötztalstraße.gpx',
-    info: 'https://www.komoot.com/smarttour/20745#og%23'
-  },
-  {
     path: 'assets/files/2019-08-11_85605933_GPX Download_ Ötztaler Stromstoß – Längenfeld Loop from Start.gpx',
     info: 'https://www.komoot.com/smarttour/433590#og%23'
-  },{
-    path: 'assets/files/2019-08-11_85606251_GPX Download_ Längenfeld – Gasthof Stuibenfall Loop from Längenfeld.gpx',
-    info: 'https://www.komoot.com/smarttour/764953#og%23'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85606420_GPX Download_ Winkelbergsee – Innerbergalm Loop from Start.gpx',
     info: 'https://www.komoot.com/smarttour/433470#og%23'
-  },{
+  }, {
     path: 'assets/files/t7728333_bike route lenzenalm -691-.gpx',
     info: 'https://www.soelden.com/summer/biking-cycling/biketours-routes/mountainbike-tours.html#7728333'
-  },{
+  }, {
     path: 'assets/files/t7728215_bike route gletscher express -648-.gpx',
     info: 'https://www.soelden.com/summer/biking-cycling/biketours-routes/mountainbike-tours.html#7728215'
-  },{
+  }, {
     path: 'assets/files/t7726482_route bodenegg race track -661-.gpx',
     info: 'https://www.soelden.com/summer/biking-cycling/biketours-routes/mountainbike-tours.html#7726482'
-  },{
+  }, {
     path: 'assets/files/t7728457_bike route - hochsoelden panorama trail.gpx',
     info: 'https://www.soelden.com/summer/biking-cycling/biketours-routes/mountainbike-tours.html#7728457'
-  },{
+  }, {
     path: 'assets/files/t7728149_bike route leiterbergalm -645-.gpx',
     info: 'https://www.soelden.com/summer/biking-cycling/biketours-routes/mountainbike-tours.html#7728149'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85607625_GPX Download_ Lochle Alpe – Kleble Alm Loop from Zentrum Shuttle.gpx',
     info: 'https://www.komoot.com/smarttour/764190#og%23'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85607755_GPX Download_ Gampe Thaya – Flowige Passage Loop from Zentrum Shuttle.gpx',
     info: 'https://www.komoot.com/smarttour/433548#og%23'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85608019_GPX Download_ Pumptrack – Sölden Trail 646 Loop from Zentrum Shuttle.gpx',
     info: 'https://www.komoot.com/smarttour/633446#og%23'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85607818_GPX Download_ Traien Trail – Ötztaler Stromstoß Loop from Zentrum Shuttle.gpx',
     info: 'https://www.komoot.com/smarttour/633532#og%23'
-  },{
-    path: 'assets/files/2019-08-11_85608155_GPX Download_ Gaißlachmittelstation – Traileingang Loop from Zentrum Shuttle.gpx',
-    info: 'https://www.komoot.com/smarttour/433553#og%23'
-  },{
+  }, {
     path: 'assets/files/2019-08-11_85608850_GPX Download_ Gaißlachmittelstation – Ebme Line Loop from Zentrum Shuttle.gpx',
     info: 'https://www.komoot.com/smarttour/433498#og%23'
-  },{
+  }, {
     path: 'assets/files/bergfextour_enduro-gletscher-express-reloaded--h.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/139105,enduro-gletscher-express-reloaded--h/'
-  },{
-    path: 'assets/files/bergfextour_biketour-hochgurgl--wurmkogl-689.gpx',
-    info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/45449,biketour-hochgurgl--wurmkogl-689/'
-  },{
-    path: 'assets/files/bergfextour_e-bike-huettentour-zur-nisslalm-634.gpx',
-    info: 'https://nl.bergfex.com/sommer/tirol/touren/radfahren/197902,e-bike-huettentour-zur-nisslalm-634/'
-  },{
+  }, {
     path: 'assets/files/bergfextour_enduro-goldegg--k.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/139101,enduro-goldegg--k/'
-  },{
+  }, {
     path: 'assets/files/bergfextour_enduro-rettenbach--n.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/139103,enduro-rettenbach--n/'
-  },{
+  }, {
     path: 'assets/files/bergfextour_enduro-schoelder-tour--m.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/139104,enduro-schoelder-tour--m/'
-  },{
+  }, {
     path: 'assets/files/bergfextour_enduro-stabele--f.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/139107,enduro-stabele--f/'
-  },{
+  }, {
     path: 'assets/files/bergfextour_mtb-soelden--kleble-stallwies-lochle-alm--fiegls-huettewindachalm.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/231401,mtb-soelden--kleble-stallwies-lochle-alm--fiegls-huettewindachalm/'
-  },{
+  }, {
     path: 'assets/files/bergfextour_enduro-stille-seite--c.gpx',
     info: 'https://nl.bergfex.com/sommer/tirol/touren/mountainbike/139098,enduro-stille-seite--c/'
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 ];
